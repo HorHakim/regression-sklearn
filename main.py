4d118d2d21ae633317d6c28e919356cec3f4cf52
Hakim Horairy <horairyhakim@gmail.com>
2024-03-08 14:37:59 +0100

    regression

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy
 import pandas
 import matplotlib.pyplot as plt
-
+import math
 from sklearn.linear_model import LinearRegression
 
 
@@ -15,10 +15,14 @@ def show_dataset(df, x_column, y_column):
 	plt.show()
 
 def prepare_data(df, y_column):
-	df["time_index"] = numpy.arange(0, len(df), 1)
-	split_index = int(len(df)*0.8)
-	train_df = df.iloc[split_index : ]
-	test_df = df.iloc[ : split_index]
+	df["time_index"] = numpy.arange(1, len(df)+1, 1)
+	split_index = int(len(df)*0.7)
+	df_mean, df_std = df["Sales"].mean(), df["Sales"].std()
+
+	df["Sales"] = (df["Sales"] - df_mean) / df_std
+
+	train_df = df.iloc[ : split_index]
+	test_df = df.iloc[split_index : ]
 
 	x_train = train_df[["time_index"]]
 	y_train = train_df[[y_column]]
@@ -26,7 +30,7 @@ def prepare_data(df, y_column):
 	x_test = test_df[["time_index"]]
 	y_test = test_df[[y_column]]
 
-	return x_train, y_train, x_test, y_test
+	return x_train, y_train, x_test, y_test, df_mean, df_std
 
 
 
@@ -37,18 +41,32 @@ def genrate_fitted_model(x_train, y_train):
 	return model
 
 
-def test_model(model, x_test, y_test):
-	y_test_predicted = model.predict(x_test)
+def test_model(model, x_test, y_test, df_mean, df_std):
+	y_test_predicted = model.predict(x_test).squeeze()
+	y_test = y_test["Sales"].values
+	rmse = math.sqrt(sum((y_test- y_test_predicted)**2/len(x_test))) # donnée normalisée
+	print(rmse)
+	
+	y_test_predicted = y_test_predicted*df_std + df_mean
+	y_test = y_test*df_std+df_mean	
+	
+
 	plt.figure(figsize=(15, 6))
-	plt.plot(x_test, y_test, "b-")
+	
+	plt.plot(x_test, y_test, "b-") # donnée originale
 	plt.plot(x_test, y_test_predicted, "r-")
+	
 	plt.show()
+
+
+
+		
 
 
 # show_dataset(dataset_df, "Years", "Sales")
 
-x_train, y_train, x_test, y_test = prepare_data(df=dataset_df, y_column="Sales")
+x_train, y_train, x_test, y_test, df_mean, df_std = prepare_data(df=dataset_df, y_column="Sales")
 
 model = genrate_fitted_model(x_train, y_train)
 
-test_model(model, x_test, y_test)
+test_model(model, x_test, y_test, df_mean, df_std)
